23f2de21b7117108ed27d72e5bf7a193311087b5
iara santos <iarasantosdev404@gmail.com>
2024-05-27 23:56:06 -0300

    inicio da v2

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ### V1: tem apenas 3 operações: Depósito, Saque e Ver extrato, trabalhando apenas com 1 usuário.
 
+### V2: o código é otimizado implementando funções, agora saque saque, depósito e extrato são funções e é adicionado mais duas: cadastro de usuário e cadastro de conta bancária(Conta corrente).
+
 
 
 
--- app.py
+++ app.py
@@ -1,59 +1,155 @@
-menu = """
-Bem vindo(a)! digite a opção da operação que deseja realizar:
+#Fução saque deve receber argumentos apenas por nome
+#a de dposito apenas por posição 
+#extrato por posição e nome, posicionais: saldo, nomeado: extrato
+#criar usuario: armazena em uma lista e tem q ter nome, data de nascimento, cpf, endereço
+# (string com formato: logradouro - numero - bairro - cidade/sigla do estado, não pode cadastrar usuarios com o mesmo cpf)
+#conta corrente: tbm armazena em uma lista sendo composta por: agência, numero da conta e usuário,
+# o numero da agencia é fixo: 0001 e o da conta é sequencial começando em 1
 
-[1] Deposito
-[2] Saque
-[3] Ver extrato
-[4] Sair
+def menu():
+    menu = """
+    Bem vindo(a)! digite a opção da operação que deseja realizar:
 
-=> """
-LIMITE_SAQUE = 500
-saldo = 0
-quant_saque = 0
-LIMITE_QUANT_SAQUE = 3
-extrato = ""
+    [1] Deposito
+    [2] Saque
+    [3] Ver extrato
+    [4] Criar Conta
+    [5] Criar usuário
+    [6] Listar contas
+    [7] Sair
+
+    => """
+    
+    return input(menu)
+
+def deposito(valor, saldo, extrato, /):
+    if valor >= 0:
+        saldo += valor
+        extrato += f"Depósito de R${valor:.2f}\n"
+        print("Valor depositado com sucesso!")
+    else:
+        print("Valor inválido! ")
+    
+    return saldo, extrato
 
 
-while True:
-    x = input(menu)
-    match (x):
-        case "1":
-            deposito = float(input("Digite o valor que você quer depositar: "))
-            if deposito >= 0:
-                saldo += deposito
-                extrato += f"Depósito de R${deposito:.2f}\n"
-                print("Valor depositado com sucesso!")
-            else:
-                print("Valor inválido! ")
-        case "2":
-            if quant_saque < LIMITE_QUANT_SAQUE:
-                saque = float(input("Digite a quantidade de dinheiro que você deseja sacar: "))
-                if saque < 0:
-                    print("Valor inválido! ")
-                    
-                elif saldo >= saque:
-                    if saque <= LIMITE_SAQUE:
-                        saldo -= saque
-                        extrato += f"Saque de R${saque:.2f}\n"
-                        print("Saque realizado com sucesso! ")
-                        quant_saque +=1
-                    else:
-                        print("Operação inválida o valor limite de saque é de R$500,00")
-                else:
-                    print("Saldo insuficiente! ")
-            else:
-                print("você já realizou os 3 saques diários ")
-        
-        case "3":
-            if extrato == "":
-                print("Nenhuma operação realizada ainda ")
-            else:
-                print("========== SEU EXTRATO ==========")
-                print(f"{extrato}\nSaldo atual: R${saldo:.2f}")
+def saque(*, valor, saldo, extrato, limite_saque = 3, max_saque = 500, quant_saque):
+    if quant_saque < limite_saque:
+        if valor < 0:
+            print("Valor inválido! ")
             
-        case "4":
-            print("Volte sempre :)")
-            break
-        case _:
-            print("Digite uma opção válida! ")
+        elif valor <= max_saque:
+            if saldo >= valor:
+                saldo -= valor
+                extrato += f"Saque de R${valor:.2f}\n"
+                print("Saque realizado com sucesso! ")
+                quant_saque +=1
+            else:
+                print("Saldo insuficiente! ")
+        else:
+            print("Operação inválida o valor limite de saque é de R$500,00")
+        
+    else:
+        print("você já realizou os 3 saques diários ")
+    
+    return saldo, extrato, quant_saque
+
+
+def exibir_extrato(saldo, /, *, extrato):
+    print("========== SEU EXTRATO ==========")
+    print("Ainda não há nada no extrato " if not extrato else extrato)
+    print(f"\nSaldo atual: R${saldo:.2f}")
+
+
+
+def cadastra_usuario(usuarios):
+    cpf = input("Digite seu CPF: ")
+    usuario = verifica_usuario(cpf, usuarios)
+    
+    if usuario:
+        print("já existe um usuário cadastrado com esse CPF! ")
+    else:
+        nome = input("Digite seu nome completo: ")
+        data_nasci = input("Digite sua data de nascimento no formato ddd/mm/aaaa: ")
+        endereco = input("E seu endereço(numero, logradouro, bairro, cidade/sigla do estado): ")
+        
+        usuarios.append({"nome": nome, "cpf": cpf, "data_nascimento": data_nasci, "endereço": endereco})
+        print("Usuário cadastrado com sucesso! ")
+
+def verifica_usuario(cpf, usuarios):
+    usuarios_filtrados = []
+    
+    for usuario in usuarios:
+        if usuario["cpf"] == cpf:
+            usuarios_filtrados.append(usuario)
+    
+    if usuarios_filtrados:
+        return usuarios_filtrados[0]
+    else:
+        return None
+
+def criar_conta(agencia, num_conta, usuarios):
+    cpf = input("Digite seu CPF: ")
+    usuario = verifica_usuario(cpf, usuarios)
+    
+    if usuario:
+        print("Conta Criada com sucesso! ")
+        return {"agencia":agencia, "numero_conta":num_conta, "usuario":usuario}
+    else:
+        print("cpf não encontrado, cadastre um usuario antes de criar uma conta! ")
+        
+        
+def listar_contas(contas):
+    for conta in contas:
+        print(f"""
+            Agência: {conta['agencia']}
+            Número da conta: {conta['numero_conta']}
+            Usuário: {conta['usuario']['nome']}
+            """)
+        print("=" * 100)
+    
+def main():
+    saldo = 0
+    extrato = ""
+    quant_saque = 0
+    
+    AGENCIA = "0001"
+    num_conta = 1
+    usuarios = []
+    contas = []
+    while True:
+        opcao = menu()
+        match opcao:
+            case "1":
+                x = float(input("Digite o valor que você quer depositar: "))
+                saldo, extrato = deposito(x, saldo, extrato)
+                
+            case "2":
+                y = float(input("Digite a quantidade de dinheiro que você deseja sacar: "))
+                saldo, extrato, quant_saque = saque(valor = y, saldo = saldo, extrato = extrato, quant_saque=quant_saque)
+                
+            
+            case "3":
+                exibir_extrato(saldo, extrato = extrato)
+            
+            case "4":
+                conta = criar_conta(AGENCIA, num_conta, usuarios)
+
+                if conta:
+                    contas.append(conta)
+                    num_conta += 1
+            
+            case "5":
+                cadastra_usuario(usuarios)   
+            
+            case "6":
+                listar_contas(contas)
+                
+            case "7":
+                print("Volte sempre :)")
+                break
+            case _:
+                print("Digite uma opção válida! ")
+
+main()
         
